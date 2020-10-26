8f5e430442e7ae5fa3854aea24f6f142ee00d77e
Pierre Sempere <pierre.sempere.01@gmail.com>
2020-10-26 23:49:03 +0100

    Update better_data.py

--- code/Testfolder/functions/better_data.py
+++ code/Testfolder/functions/better_data.py
@@ -1,7 +1,13 @@
 
 def Better_insert_data(cursor, ObjectList, target_table, cols, val_names, cnx):
+
+    attr_names= ""
+    key_list = ObjectList[0].__dict__.keys()
+    for key in key_list:
+        print(key, type(key))
+        attr_names = attr_names+key+", "
+
     for element in ObjectList:
         data_insertion = f"""INSERT INTO {target_table}
-        ({cols})
+        ({attr_names})
         VALUES({vals})"""
-
